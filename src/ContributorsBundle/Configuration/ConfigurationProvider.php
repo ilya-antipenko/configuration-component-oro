c72fac189341fd8eeba62106b5552368b29c88bb
Ilya Antipenko <ilya@antipenko.pp.ua>
2018-09-20 13:09:43 +0300

    Add caching

--- src/ContributorsBundle/Configuration/ConfigurationProvider.php
+++ src/ContributorsBundle/Configuration/ConfigurationProvider.php
@@ -3,6 +3,7 @@
 namespace ContributorsBundle\Configuration;
 
 use ContributorsBundle\Configuration\ContributorsConfiguration;
+use Doctrine\Common\Cache\CacheProvider;
 use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\Config\Exception\FileLocatorFileNotFoundException;
 use Symfony\Component\Config\FileLocator;
@@ -15,15 +16,36 @@ class ConfigurationProvider
      */
     private $bundles;
 
-    public function __construct(array $bundles)
+    /**
+     * @var CacheProvider
+     */
+    private $cacheProvider;
+
+    public function __construct(array $bundles, CacheProvider $cacheProvider)
     {
         $this->bundles = $bundles;
+        $this->cacheProvider = $cacheProvider;
     }
 
     /**
      * @param string $filePath Example: Resources/config/contributors.yml
+     * @return array
      */
-    public function load(string $filePath)
+    public function load(string $filePath): array
+    {
+        $cachedConfig = $this->cacheProvider->fetch($filePath);
+
+        if ($cachedConfig) {
+            return $cachedConfig;
+        }
+
+        $resolvedConfig = $this->resolveConfiguration($filePath);
+        $this->cacheProvider->save($filePath, $resolvedConfig);
+
+        return $resolvedConfig;
+    }
+
+    private function resolveConfiguration($filePath): array
     {
         $configurationFiles = $this->findFiles($filePath);
 
@@ -38,7 +60,7 @@ class ConfigurationProvider
         return $processedConfig;
     }
 
-    private function findFiles(string $filePath)
+    private function findFiles(string $filePath): array
     {
         $paths = [];
         foreach ($this->bundles as $bundleName => $bundleClass) {
